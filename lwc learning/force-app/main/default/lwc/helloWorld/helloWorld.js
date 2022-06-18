164086afc8250ecf224f2a097549d3b48ad451ec
Anna Eunbi Seo <49229333+eunbibi@users.noreply.github.com>
2022-06-18 02:22:51 -0400

    Add files via upload

---
+++ lwc learning/force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -0,0 +1,21 @@
+import { LightningElement } from 'lwc';
+
+export default class HelloWorld extends LightningElement {
+//property, methods
+    /*
+    name //undefined
+    age=30
+    fullname  = "salesforce Canada"
+    showData = false
+    details={
+        name:"sweety",
+        place:"toronto"
+    }
+    userList =["a", "b", "C"]
+    */
+
+    fullname = "Zero to Hero"
+    object = {
+        name: "anna"
+    }
+}
