1b11e9316790772fd1a432fc7d8bafe1a56b9695
Anna Eunbi Seo <49229333+eunbibi@users.noreply.github.com>
2022-06-20 17:26:58 -0400

    Update helloWorld.js

--- lwc learning/force-app/main/default/lwc/helloWorld/helloWorld.js
+++ lwc learning/force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -1,21 +1,40 @@
 import { LightningElement } from 'lwc';
 
 export default class HelloWorld extends LightningElement {
-//property, methods
+
+    //1. property
     /*
     name //undefined
     age=30
     fullname  = "salesforce Canada"
     showData = false
+
+    //object  
     details={
         name:"sweety",
         place:"toronto"
     }
+
+    //array
     userList =["a", "b", "C"]
     */
 
     fullname = "Zero to Hero"
     object = {
         name: "anna"
+    }
+    user = ["anna", "bnna", "cana"]
+
+
+    //2. method
+    /*
+    getName(){
+        //perform my logic
+
+    }*/
+    title = "aura"
+
+    changeHandler(event){
+        this.title = event.target.value
     }
 }
